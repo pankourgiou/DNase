3208a56ed9c0bcfc317e76591a84df2986b90b66
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-01-30 23:02:55 +0200

    Create Laocoon.scm

---
+++ Laocoon.scm
@@ -0,0 +1,10 @@
+(string=? "Laocoön's silence is silent" "Laocoön's silence is silent")      ;; => #t
+(string=? "Achilles is sometimes silent" "Achilles is sometimes silent")  ;; => #t
+(string=? "Achilles acts simillarly to Laocoön" "Achilles acts simillarly to Laocoön")     
+(string=? "Achilles needs a knife"  "Achilles needs a knife")
+(string=? "So does Laocoön"  "So does Laocoön") 
+(string=? "But books are like spoons"  "But books are like spoons") 
+(string=? "Haha"  "Haha")
+(string=? "Odysseas says: why don't you use a laser?"  "Odysseas says: why don't you use a laser?") 
+(string=? "Achilles says:you can't teach an old dog new tricks" "Achilles says:you can't teach an old dog new tricks")
+(string=? "Smiling makes us young and healthy and it's free so smile" "Smiling makes us young and healthy and it's free so smile")
