5a6e8b6113bb5cc3c40bf5aa55058a6616c11fdf
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2022-10-05 07:29:39 +0300

    Update and rename achtzehn.py to DNase.R

---
+++ DNase.R
@@ -0,0 +1,16 @@
+DNase
+require(stats); require(graphics)
+
+coplot(density ~ conc | Run, data = DNase,
+       show.given = FALSE, type = "b")
+coplot(density ~ log(conc) | Run, data = DNase,
+       show.given = FALSE, type = "b")
+## fit a representative run
+fm1 <- nls(density ~ SSlogis( log(conc), Asym, xmid, scal ),
+    data = DNase, subset = Run == 1)
+## compare with a four-parameter logistic
+fm2 <- nls(density ~ SSfpl( log(conc), A, B, xmid, scal ),
+    data = DNase, subset = Run == 1)
+summary(fm2)
+anova(fm1, fm2)
+
